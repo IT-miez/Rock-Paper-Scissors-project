b021035f8b29e5234d8a18f833dcfcc6d640d4a1
Veikko Moilanen <veikkosakari01@gmail.com>
2023-05-31 13:38:25 +0300

    Rock Paper Scissors - game version 1.0. Modified UI, added restart button after game ends.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Simple Rock, Paper, Scissors - game
-I made this project as part of doing tasks in The Odin Project.
+# The Odin Project -  Rock, Paper, Scissors game
+This project was made while doing The Odin Project. </br>
+I made a couple iterations before I completed my rock paper scissors website with clickable hovering icons.</br><br>
+
+## Technologies used
+**HTML** </br>
+**CSS** (styling, flexbox, hover-effects)</br>
+**JavaScript** (results, popup, game-code, DOM-modification)</br>
+
+## What I learned
+I used my skills on **flexboxes** when structuring the site. I used **JavaScript** to **manipulate the DOM-elements when the game goes on**, adding results, stopping buttons from working when 5 wins has occurred for the player or computer and a popup to let the user know who won.</br>
+In this project I **refreshed my JavaScript-skills**, **and** got to use **flexboxes** again when structuring the website.
+
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 
 
+
+resultDiv = document.getElementById("resultdiv");
+restartBtn = document.getElementById("restartButton")
+
+//rps_buttons = document.querySelectorAll(".chooseButton")
+rps_buttons = document.querySelectorAll(".buttonWrapper")
+let btnsArr = Array.from(rps_buttons)
+console.log(btnsArr)
+
+// COUNTERS
+let gameCounter = 0
+let playerWins = 0
+let computerWins = 0
+
 function playRound(playerSelection, computerSelection) {
     // your code here!
-
+    // ADD 1 TO GAMECOUNTER
+    gameCounter = gameCounter + 1
     // make all uppercase so it is case insensitive
     playerSelection = playerSelection.toUpperCase()
     computerSelection = computerSelection.toUpperCase()
 
+    let resultText;
 
     if(playerSelection == "ROCK" || playerSelection == "SCISSORS" || playerSelection == "PAPER") {
     }
@@ -16,31 +32,44 @@ function playRound(playerSelection, computerSelection) {
     }
 
     if (playerSelection == computerSelection){
-        console.log("It's a tie!")
+        resultText = ("It's a tie!")
+        console.log(resultText)
     }
 
 
     else if(playerSelection == "ROCK" && computerSelection != "SCISSORS" ) {
-        console.log(computerSelection + " wins " + playerSelection + ", you lose!")
+        resultText = (computerSelection + " wins " + playerSelection + ", you lose!")
+        computerWins = computerWins + 1 
     }
 
     else if(playerSelection == "PAPER" && computerSelection != "ROCK") {
-        console.log(computerSelection + " wins " + playerSelection + ", you lose!")
+        resultText = (computerSelection + " wins " + playerSelection + ", you lose!")
+        computerWins = computerWins + 1 
     }
 
     else if(playerSelection == "SCISSORS" && computerSelection != "PAPER") {
-        console.log(computerSelection + " wins " + playerSelection + ", you lose!")
+        resultText = (computerSelection + " wins " + playerSelection + ", you lose!")
+        computerWins = computerWins + 1 
     }
 
     
     else {
-        console.log(playerSelection + " wins " + computerSelection + ", you win!")
+        resultText = (playerSelection + " wins " + computerSelection + ", you win!")
+        playerWins = playerWins + 1 
     }
-    console.log(playerSelection, computerSelection)
-  }
-   
-  
 
+    // ADD RESULT TO SITE
+    addResultDivValue("Round "+gameCounter + ": "+resultText, playerWins, computerWins)
+    gameEndedCheck(gameCounter, playerWins, computerWins)
+
+
+    // Player and Computer choice console.log for debugging
+    //console.log(playerSelection, computerSelection)
+}
+
+  
+/* OLD FUNCTION FOR THE GAME ON
+    BROWSER CONSOLE
 function game() {
     console.log("Welcome to play ROCK PAPER SCISSORS!")
     for(let i = 0; i < 5; i++) {
@@ -50,5 +79,114 @@ function game() {
         playRound(userInput, computerInput)
     }
 }
+*/
+//game()
 
-game()
+
+function addResultDivValue (resultText, playerWins, computerWins) {
+
+    
+    resultDiv_base = document.getElementById("resultdiv-base")
+    resultDiv = document.createElement("div")
+    resultDiv.classList.add("resultdiv")
+    new_p = document.createElement("p")
+    new_p.textContent = resultText
+    new_p.classList.add("resultText")
+
+    /*
+    second_p = document.createElement("p")
+    second_p.textContent = "Status: Player "+playerWins +" - "+computerWins + " Computer"
+    second_p.classList.add("resultText-status")
+    */
+
+    resultDiv.appendChild(new_p)
+    //resultDiv.appendChild(second_p)
+    resultDiv_base.appendChild(resultDiv)
+    
+
+    header = document.getElementById("secondary-header")
+    console.log(header)
+    console.log("apina")
+    resultText = "Result: "+playerWins+ " - "+ computerWins
+    header.textContent = resultText
+}
+
+/* OLD EVENT LISTENER FOR BUTTON 
+function addEventListenerList() {
+    for (let i = 0, len = btnsArr.length; i < len; i++) {
+        console.log(btnsArr[i])
+        btnsArr[i].addEventListener("click", () => {
+            console.log("nappi")
+            console.log(btnsArr[i].textContent)
+            playRound(btnsArr[i].textContent, "rock")
+        })
+    }
+}
+*/
+
+function addEventListenerForIconsList() {
+    for (let i = 0, len = btnsArr.length; i < len; i++) {
+        console.log(btnsArr[i])
+        btnsArr[i].addEventListener("click", () => {
+            console.log("icon + button")
+            console.log(btnsArr[i]["children"][1].textContent)
+
+            // RANDOMIZE COMPUTER CHOOSING
+            let rps_list = ["Rock", "Paper", "Scissors"]
+            computerChoice = Math.floor((Math.random() * 3))
+            console.log("Computer chooses "+rps_list[computerChoice])
+
+
+            playRound(btnsArr[i]["children"][1].textContent, rps_list[computerChoice])
+        })
+    }
+}
+
+function gameEndedCheck (gameCounter, playerWins, computerWins) {
+    console.log(gameCounter, playerWins, computerWins)
+    let gameOverText, endResultText;
+    if(playerWins == 5) {
+        gameOverText = "You won!"
+        endResultText = "Result: "+playerWins+" - "+computerWins
+        console.log(endResultText)
+        // DISABLE BUTTONS
+        for (let i = 0, len = btnsArr.length; i < len; i++) {
+            console.log(btnsArr[i])
+            btnsArr[i].classList.add("disabled-clicks")
+        }
+        
+
+        // ADD HEADER TEXT
+        header = document.getElementById("secondary-header")
+        console.log(header)
+        console.log("apina")
+        header.textContent = endResultText
+        restartBtn.classList.toggle("extended")
+        alert(gameOverText)
+    }
+    else if (computerWins == 5) {
+        gameOverText = "You lost!"
+        endResultText = "Result: "+playerWins+" - "+computerWins
+        for (let i = 0, len = btnsArr.length; i < len; i++) {
+            console.log(btnsArr[i])
+            btnsArr[i].classList.add("disabled-clicks")
+        }
+        
+        // ADD HEADER TEXT
+        header = document.getElementById("secondary-header")
+        console.log(header)
+        console.log("apina")
+        header.textContent = endResultText
+        restartBtn.classList.toggle("extended")
+        alert(gameOverText)
+    }
+
+    console.log(gameCounter, playerWins, computerWins)
+
+}
+
+restartBtn.addEventListener("click", () => {
+    location.reload()
+})
+
+addEventListenerForIconsList()
